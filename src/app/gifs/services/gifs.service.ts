189f6254b92a04b0491cc6b1640473d058c96d36
Juanjo <juanjomp85gmail.com>
2024-01-09 16:21:33 +0100

    GifsApp

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { SearchResponse, Gif } from '../interfaces/gifs.interfaces';
+
+@Injectable({ providedIn: 'root' })
+export class GifsService {
+
+  public gifList: Gif[] = [];
+
+  private _tagsHistory: string[] = [];
+  private apiKey:       string = 'kTRMM05MtcpCTviIWBs3An1PR1QGAWJK';
+  private serviceUrl:   string = 'https://api.giphy.com/v1/gifs';
+
+  constructor( private http: HttpClient ) {
+    this.loadLocalStorage();
+    console.log('Gifs Service Ready');
+  }
+
+  get tagsHistory() {
+    return [...this._tagsHistory];
+  }
+
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if ( this._tagsHistory.includes( tag ) ) {
+      this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag !== tag )
+    }
+
+    this._tagsHistory.unshift( tag );
+    this._tagsHistory = this.tagsHistory.splice(0,10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage():void {
+    localStorage.setItem('history', JSON.stringify( this._tagsHistory ));
+  }
+
+  private loadLocalStorage():void {
+    if( !localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse( localStorage.getItem('history')! );
+
+    if ( this._tagsHistory.length === 0 ) return;
+    this.searchTag( this._tagsHistory[0] );
+  }
+
+  eraseTags(): void {
+    if( localStorage.getItem('history')) {
+      console.log(this._tagsHistory);
+      this._tagsHistory.pop();
+    }
+  }
+
+  searchTag( tag: string ):void {
+    if ( tag.length === 0 ) return;
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey )
+      .set('limit', '20' )
+      .set('q', tag )
+
+    this.http.get<SearchResponse>(`${ this.serviceUrl }/search`, { params })
+      .subscribe( resp => {
+
+        this.gifList = resp.data;
+        // console.log({ gifs: this.gifList });
+
+      });
+
+
+
+
+
+
+  }
+
+
+}
